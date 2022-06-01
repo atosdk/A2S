b9b6b63ce9988e04c341753f28e6806f906985aa
Miron Srebrakowski <miron.srebrakowski@atos.net>
2022-06-01 11:05:28 +0100

    completed events lwc

--- force-app/main/default/lwc/birthdaySearch/birthdaySearch.js
+++ force-app/main/default/lwc/birthdaySearch/birthdaySearch.js
@@ -1,12 +1,45 @@
-import { LightningElement, wire, api } from 'lwc';
-import getBirthdays from '@salesforce/apex/A2S_UserController.getBirthdays';
+import { LightningElement, api, wire } from "lwc";
+import getBirthdays from "@salesforce/apex/A2S_UserController.getBirthdays";
 
 export default class BirthdaySearch extends LightningElement {
+  d = new Date();
+  currMonth = this.d.getMonth();
+  allMonths = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
 
-    @api value = 11;
-    @api label = 'November';
-    
-    @wire(getBirthdays, {month: '$value' }) users;
+  @api value = this.currMonth;
+  @api label = this.allMonths[this.currMonth];
+  @api team = "Salesforce";
 
-    
+  @wire(getBirthdays, { month: "$value", team: "$team" }) users;
+
+  /*
+    @track users;
+
+    @wire(getBirthdays, {month: '$value', team:'$team' }) 
+    objcurr({ data, error }) {
+        if (data) {
+            this.users = data;
+            this.displayUserstoConsole(this.users);
+        } else if (error) {
+            console.log(error);
+        }
+    }
+
+    displayUserstoConsole(currData) {
+        console.log('currdata', JSON.stringify(currData));
+    }
+    */
 }
