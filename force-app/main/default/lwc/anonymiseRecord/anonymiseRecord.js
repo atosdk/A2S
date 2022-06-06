cbbb262bebac44064cb836173d059179e1ae6ba5
Miron Srebrakowski <47178615+miron-srebrakowski@users.noreply.github.com>
2022-06-06 13:34:37 +0100

    added field selection

--- force-app/main/default/lwc/anonymiseRecord/anonymiseRecord.js
+++ force-app/main/default/lwc/anonymiseRecord/anonymiseRecord.js
@@ -1,10 +1,43 @@
-import { LightningElement, api } from "lwc";
+import { LightningElement } from "lwc";
+import getFields from "@salesforce/apex/A2S_FieldSelectionHandler.getFields";
 
 export default class AnonymiseRecord extends LightningElement {
-  @api fields = [
-    { id: 1, name: "Name1" },
-    { id: 2, name: "Name2" },
-    { id: 3, name: "Name3" },
-    { id: 4, name: "Name4" }
-  ];
+  options = [];
+  values = [];
+  requiredOptions = [];
+  isFirst = true;
+  _fields = [];
+
+  async connectedCallback() {
+    await this.fetchData();
+  }
+
+  get fields() {
+    return this._fields.length ? this._fields : "";
+  }
+
+  handleChange(e) {
+    this._fields = e.detail.value;
+  }
+
+  async fetchData() {
+    await getFields({
+      objectname: "User"
+    })
+      .then((result) => {
+        let data = JSON.parse(JSON.stringify(result));
+        let lstOption = [];
+        for (let i = 0; i < data.length; i++) {
+          lstOption.push({
+            value: data[i].QualifiedApiName,
+            label: data[i].DeveloperName
+          });
+        }
+        this.options = lstOption;
+        console.log(lstOption);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 }
