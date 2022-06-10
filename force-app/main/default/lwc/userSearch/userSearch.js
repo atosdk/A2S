4e6871704edfb562b7cf432345d080dbcea4a379
Miron Srebrakowski <47178615+miron-srebrakowski@users.noreply.github.com>
2022-06-10 11:41:26 +0100

    .

--- force-app/main/default/lwc/userSearch/userSearch.js
+++ force-app/main/default/lwc/userSearch/userSearch.js
@@ -1,26 +1,37 @@
-import { LightningElement, track, wire } from 'lwc';
+import { LightningElement, api } from "lwc";
 import getUsers from "@salesforce/apex/A2S_UserSearch.getUsers";
 
 export default class UserSearch extends LightningElement {
-    key;
-    @track users;
-    updateKey(event){
-        this.key = event.target.value;
-    }
+  @api objectname;
+  key;
+  //@track users;
+  updateKey(event) {
+    this.key = event.target.value;
+  }
+  users = [];
 
-    @wire(getUsers, {searchKey: '$key'}) users;
+  //@wire(getUsers, {searchKey: '$key', objectname : this.objectname }) users;
 
-    // handleSearch(){
-    //     getUsers({searchKey: this.key})
-    //     .then(result=>{
-    //         this.users = result;
-    //     })
-    //     .catch(error=>{
-    //         this.users = null;
-    //     });
-    // }
+  handleClick() {
+    console.log(this.objectname);
 
-    cols = [
-        {label: 'Name', fieldName: 'Name', type: 'text'}
-    ]
+    getUsers({ searchKey: this.key, objectname: this.objectname })
+      .then((res) => {
+        this.users = res;
+        console.log(this.users.data);
+      })
+      .catch((err) => console.error(err));
+  }
+
+  // handleSearch(){
+  //     getUsers({searchKey: this.key})
+  //     .then(result=>{
+  //         this.users = result;
+  //     })
+  //     .catch(error=>{
+  //         this.users = null;
+  //     });
+  // }
+
+  cols = [{ label: "Name", fieldName: "Name", type: "text" }];
 }
