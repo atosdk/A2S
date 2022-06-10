6e71b3641d5b7efb3c551e2791f7a25b4582a508
Miron Srebrakowski <47178615+miron-srebrakowski@users.noreply.github.com>
2022-06-10 14:03:04 +0100

    full functionality. TODO: add custom fields

--- force-app/main/default/lwc/anonymiseRecord/anonymiseRecord.js
+++ force-app/main/default/lwc/anonymiseRecord/anonymiseRecord.js
@@ -1,6 +1,5 @@
 import { LightningElement } from "lwc";
 import anonymiseFields from "@salesforce/apex/A2S_FieldSelectionHandler.anonymiseFields";
-import getFields from "@salesforce/apex/A2S_FieldSelectionHandler.getFields";
 
 export default class AnonymiseRecord extends LightningElement {
   fields = [
@@ -12,12 +11,24 @@ export default class AnonymiseRecord extends LightningElement {
   ];
 
   objects = ["User", "Account", "Contact", "Lead", "CampaignMember"];
+  selectedIds = [];
+  lastObject;
   confirmed = false;
 
   handleFieldsChange(e) {
     this.fields[this.objects.indexOf(e.detail.objectname)].selected = [
       ...e.detail.selected
     ];
+
+    this.lastObject = e.detail.objectname;
+  }
+
+  handleSelectedId(event) {
+    const selectedRecords = event.detail.value;
+
+    console.log("Top; " + selectedRecords[0].Id);
+
+    this.selectedIds = selectedRecords;
   }
 
   handelCheckbox(e) {
@@ -39,30 +50,15 @@ export default class AnonymiseRecord extends LightningElement {
       console.log("Ok");
     } else {
       anonymiseFields({
-        objectname: "Account",
-        selectedfields: this.fields[1].selected
+        objectname: this.lastObject,
+        selectedfields:
+          this.fields[this.objects.indexOf(this.lastObject)].selected,
+        objectid: this.selectedIds[0].Id
       })
         .then((res) => {
           console.log(res);
         })
         .catch((err) => console.log(err.body.message));
     }
-  }
-
-  async fetchData(object) {
-    await getFields({
-      objectname: object
-    })
-      .then((result) => {
-        let data = JSON.parse(JSON.stringify(result));
-        let lstOption = [];
-        for (let i = 0; i < data.length; i++) {
-          lstOption.push(data[i].DeveloperName);
-        }
-        this.options = lstOption;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
   }
 }
