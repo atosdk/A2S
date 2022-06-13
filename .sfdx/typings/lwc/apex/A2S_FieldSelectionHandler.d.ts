897ee66cfadac42b069f6c83d6ca515f86a06a4a
Jack Quinn <jackquinn2@msn.com>
2022-06-13 16:40:42 +0100

    added unit tests

--- .sfdx/typings/lwc/apex/A2S_FieldSelectionHandler.d.ts
+++ .sfdx/typings/lwc/apex/A2S_FieldSelectionHandler.d.ts
@@ -1,3 +1,6 @@
 declare module "@salesforce/apex/A2S_FieldSelectionHandler.getFields" {
-  export default function getFields(param: {objectname: any}): Promise<any>;
+  export default function getFields(param: {objectname: any, objectid: any}): Promise<any>;
+}
+declare module "@salesforce/apex/A2S_FieldSelectionHandler.anonymiseFields" {
+  export default function anonymiseFields(param: {objectname: any, selectedfields: any, objectid: any}): Promise<any>;
 }
