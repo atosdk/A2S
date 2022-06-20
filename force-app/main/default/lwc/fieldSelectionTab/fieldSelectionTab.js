20b77fbd9729bc336cf9ba1486109bfb40f21be3
miron-srebrakowski <miron.srebrakowski@gmail.com>
2022-06-20 10:33:20 +0100

    notifications

--- force-app/main/default/lwc/fieldSelectionTab/fieldSelectionTab.js
+++ force-app/main/default/lwc/fieldSelectionTab/fieldSelectionTab.js
@@ -8,6 +8,16 @@ export default class FieldSelectionTab extends LightningElement {
   requiredOptions = [];
   isFirst = true;
   fields;
+  selectedRecords = [];
+  requiredFieldNames = [
+    "FirstName",
+    "LastName",
+    "Name",
+    "Phone",
+    "Email",
+    "Gender",
+    "DOB__c"
+  ];
 
   async connectedCallback() {
     await this.fetchData();
@@ -26,22 +36,56 @@ export default class FieldSelectionTab extends LightningElement {
     this.dispatchEvent(selectedEvent);
   }
 
+  handleSelectedRecords(event) {
+    const selectedRecords = event.detail.value;
+
+    console.log("Mid: " + selectedRecords[0].Id);
+    this.selectedIds = selectedRecords;
+
+    // eslint-disable-next-line guard-for-in
+    // for(let id in event.detail.value) {
+
+    //     this.selectedIds.push(id.Id);
+
+    // }
+
+    this.fetchData();
+
+    const selectedEvent = new CustomEvent("selectedid", {
+      detail: { value: this.selectedIds }
+    });
+
+    this.dispatchEvent(selectedEvent);
+  }
+
   async fetchData() {
     await getFields({
-      objectname: this.objectname
-      //objectid : this.id
+      objectname: this.objectname,
+      objectid: this.selectedIds[0].Id
     })
       .then((result) => {
         let data = JSON.parse(JSON.stringify(result));
         let lstOption = [];
+        let lstRequiredOptions = [];
+
         for (let i = 0; i < data.length; i++) {
+          if (this.requiredFieldNames.includes(data[i])) {
+            lstRequiredOptions.push({
+              value: data[i],
+              label: data[i]
+            });
+          }
           lstOption.push({
             value: data[i],
             label: data[i]
           });
         }
         this.options = lstOption;
-        console.log(lstOption);
+        this.requiredOptions = lstRequiredOptions;
+        //this.values = lstRequiredOptions;
+
+        console.log(this.requiredOptions);
+        console.log(this.options);
       })
       .catch((error) => {
         console.error(error);
